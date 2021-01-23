a722304f1c42b5817da8e6f44bbadb8cfea02869
Paul T <paultoro101@gmail.com>
2021-01-23 15:08:46 -0500

    fixed links

---
+++ README.md
@@ -0,0 +1,13 @@
+Deployment & Repository
+Heroku Deployment
+Github Repository Link
+
+Technical Requirements
+Javascript
+React.js
+JSX
+Express/Node.js
+MongoDB/Mongoose
+
+Installation:
+This package requires node.js and Inquirer. You can install the package dependencies by entering the following commands: "npm install" or "npm i".
---
+++ client/build/precache-manifest.1a872dfd9558f3516b0d26edd67d756d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "488343ad0457fc30d731",
+    "url": "/static/css/main.4a9d446f.chunk.css"
+  },
+  {
+    "revision": "488343ad0457fc30d731",
+    "url": "/static/js/main.bd559d1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "630481b45022ad51fa86",
+    "url": "/static/js/2.e8c4bf92.chunk.js"
+  },
+  {
+    "revision": "bda7d4d586b0fee21134aabaf2e9be9e",
+    "url": "/index.html"
+  }
+];
